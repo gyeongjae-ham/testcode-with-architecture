dce3f5d3c0476ecd9b38ea08fb0b52e5c488f065
howdy-luca <gyeongjae.h.dev@gmail.com>
2023-06-24 00:27:59 +0900

    :art: Improve structure and refactor package name, class name

--- test/com/example/demo/controller/UserCreateControllerTest.java
+++ test/com/example/demo/controller/UserCreateControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.user.domain.UserCreate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
@@ -39,7 +39,7 @@ public class UserCreateControllerTest {
     @Test
     void 사용자는_회원_가입을_할_수있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
         // Given
-        UserCreateDto userCreateDto = UserCreateDto.builder()
+        UserCreate userCreate = UserCreate.builder()
                 .email("kok202@kakao.com")
                 .nickname("kok202")
                 .address("Pangyo")
@@ -52,7 +52,7 @@ public class UserCreateControllerTest {
                         post("/api/users")
                                 .header("EMAIL", "kok202@naver.com")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(userCreateDto)))
+                                .content(objectMapper.writeValueAsString(userCreate)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").isNumber())
                 .andExpect(jsonPath("$.email").value("kok202@kakao.com"))
